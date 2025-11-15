aaa29d85d15e0af9c2d44167fb94a22fdc395f5d
Sadikhova-A <aylinsadixova@gmail.com>
2025-11-15 17:17:07 +0000

    Separated data into train, test and valid. Plus readability changes

--- Data/dataAnalysis.py
+++ Data/dataAnalysis.py
@@ -2,25 +2,28 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 
-# Read and diplay the dataset
-df = pd.read_csv("Data\\creditcard.csv")
-# print(df.head())
-# print(df["Class"].unique())  # Out: [0, 1], Data is already in factors
+# Read the dataset
+df = pd.read_csv("Data\\creditcard.csv")  # Source: kagglehub - credit card fraud
 
-# Plotting the features of the dataset to see the differences betweem fraud and legitimate transactions
-fig, ax = plt.subplots(5, 6, figsize = (10, 9), layout = 'constrained')  # 5x6 Subplots
+# Just a test funtion to view the data and formatting
+def view():  
+    print(df.head())
+    print(df["Class"].unique())  # Out: [0, 1], Data is already in factors
 
-for (i, cols) in enumerate(df.columns[:-1]):
-    ax[i//6, i%6].hist(df[df["Class"] == 1][cols],  color = 'red', label='fraud', alpha=0.65, density=True)  # Fraudulent
-    ax[i//6, i%6].hist(df[df["Class"] == 0][cols],  color = 'blue', label='legitamte', alpha=0.65, density=True)
-    ax[i//6, i%6].set_title(cols, fontsize=8)
-    ax[i//6, i%6].legend(fontsize=8)
+# Plotting the features of the dataset to see the differences between fraud and legitimate transactions
+def saveDiffPlot():
+    fig, ax = plt.subplots(5, 6, figsize = (10, 9), layout = 'constrained')  # 5x6 Subplots
 
-plt.show()
-plt.close()
+    # A loop for plotting histograms with the 30 features. Using i // 6 for rows and i mod 6 for colunms
+    for (i, cols) in enumerate(df.columns[:-1]):
+        ax[i//6, i%6].hist(df[df["Class"] == 1][cols],  color = 'red', label='fraud', alpha=0.65, density=True)  # Fraudulent
+        ax[i//6, i%6].hist(df[df["Class"] == 0][cols],  color = 'blue', label='legitimate', alpha=0.65, density=True)
+        ax[i//6, i%6].set_title(cols, fontsize=8)
+        ax[i//6, i%6].legend(fontsize=8)
 
-y = df[df.columns[-1]].values
-print(y)
+    # Displaying the plot
+    fig.suptitle('Visual difference: Fraudulent and Legitimate transactions')
+    plt.savefig('Data\\Visual_difference-Transactions.png')
 
-Y = np.reshape(y, (-1, 1))
-print(Y)
+view()
+saveDiffPlot()
